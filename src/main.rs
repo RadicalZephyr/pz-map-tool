b431d22ee411bdf68e1740781788dcf84b5668d9
Zefira Shannon <earthlingzephyr@gmail.com>
2024-01-09 00:39:08 -0800

    Parse basic arguments for the save file source

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,36 @@
-use clap::Parser;
+use std::path::PathBuf;
+
+use clap::{Args, Parser};
 
 #[derive(Debug, Parser)]
-struct Args {
+#[command(author, version, about, long_about)]
+struct Cli {
     /// Turn debugging information on
     #[arg(short, long, action = clap::ArgAction::Count)]
     debug: u8,
+
+    /// Print which files would be removed without doing anything
+    #[arg(long)]
+    dry_run: bool,
+
+    #[command(flatten)]
+    source: Source,
+}
+
+#[derive(Clone, Debug, Args)]
+#[group(required = true, multiple = false)]
+struct Source {
+    /// Specify the save file name
+    #[arg(short, long, group = "source")]
+    name: Option<String>,
+
+    /// Specify the path to the save file
+    #[arg(short, long, group = "source")]
+    path: Option<PathBuf>,
 }
 
 fn main() {
-    let args = Args::parse();
+    let args = Cli::parse();
 
     println!("{args:#?}");
 }
