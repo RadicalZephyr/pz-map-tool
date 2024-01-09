0c540434cd4db7976a58eda11a4d6c1f2753f16a
Zefira Shannon <earthlingzephyr@gmail.com>
2024-01-09 08:37:26 -0800

    Sketch out getting path to save by save name

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::path::PathBuf;
 
-use clap::{Args, Parser};
+use clap::{Args, CommandFactory, Parser};
 use pz_map_tool::{Action, MapRegion, Source};
 
 #[derive(Debug, Parser)]
@@ -68,6 +68,20 @@ fn main() {
         region,
     } = cli;
 
+    match source.into_enum() {
+        Source::SaveName(save_name) => {
+            let Some(mut home) = dirs::home_dir() else {
+                let mut cmd = Cli::command();
+                cmd.error(
+                    clap::error::ErrorKind::ValueValidation,
+                    "save name specified, but could not find a home directory, please use --path instead."
+                ).exit();
+            };
+            let mut saves_path = home;
+            saves_path.extend(["Zomboid", "Saves"]);
+        }
+        Source::Path(path) => todo!(),
+    }
     // Get the Path for the save file by name or direct path
     //   - Verify it's a valid save name/path
 
